d6ec402982e40477099dc99c01acd11a6a736592
Larry <26318510+lyu18@users.noreply.github.com>
2020-12-02 13:10:06 -0600

    Verify contracts on Etherscan using Hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+const { INFURA_PROJECT_ID, MNEMONIC, ETHERSCAN_API_KEY } = require('./secrets.json');
+
+require("@nomiclabs/hardhat-etherscan");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: { mnemonic: MNEMONIC }
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: { mnemonic: MNEMONIC }
+    },
+  },
+  etherscan: { apiKey: ETHERSCAN_API_KEY },
+  solidity: "0.7.5"
+};
