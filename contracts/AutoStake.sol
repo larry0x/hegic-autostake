a83e065b2d802017dbf454467ce39ddc40b38e68
Larrypcdotcom <26318510+Larrypcdotcom@users.noreply.github.com>
2020-12-06 22:26:40 -0600

    Using abstract contract

---
+++ contracts/AutoStake.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+// import "./interfaces/IHegicPoolV2.sol";
+import "./interfaces/IIOUTokenRedemption.sol";
+
+
+abstract contract AutoStake is Ownable {
+    using SafeMath for uint;
+    using SafeERC20 for IERC20;
+
+    struct UserData {
+        uint amountDeposited;
+        uint amountWithdrawn;
+    }
+
+    IERC20 public HEGIC;
+    IERC20 public rHEGIC;
+    IIOUTokenRedemption public redemption;
+
+    uint public feeRate = 100;  // 1%
+    address public feeRecipient;
+
+    bool public allowDeposit = true;
+    bool public allowClaimRefund = true;
+
+    uint public totalDepositors = 0;
+    uint public totalDeposited = 0;
+    uint public totalRedeemed = 0;
+    uint public totalWithdrawable = 0;
+    uint public totalWithdrawn = 0;  // Not Including fees
+    uint public totalFeeCollected = 0;
+
+    uint public lastRedemptionTimestamp;
+
+    mapping(address => UserData) public userData;
+
+    event Deposited(address account, uint amount);
+    event RefundClaimed(address account, uint amount);
+    event Withdrawn(address account, uint amountAfterFee, uint fee);
+
+    /**
+     * @notice Set the fee rate users are charged upon withdrawal.
+     * @param _feeRate The new rate in basis points. E.g. 200 = 2%
+     */
+    function setFeeRate(uint _feeRate) external onlyOwner {
+        require(_feeRate >= 0, "Rate too low!");
+        require(_feeRate <= 500, "Rate too high!");
+        feeRate = _feeRate;
+    }
+
+    /**
+     * @notice Set the recipient address to fees generated.
+     * @param _feeRecipient The new recipient address
+     */
+    function setFeeRecipient(address _feeRecipient) external onlyOwner {
+        require(_feeRecipient != address(0), "Cannot set recipient to zero address");
+        feeRecipient = _feeRecipient;
+    }
+
+    /**
+     * @notice Set to accept or reject new deposits. May be called if the project
+     * fails to attract enough deposits that justify the work. In this case the
+     * developer will inform depositors to withdraw their rHEGIC by calling the
+     * `claimRefund` function.
+     * @param _allowDeposit Whether new deposits are accepted
+     */
+    function setAllowDeposit(bool _allowDeposit) external onlyOwner {
+        allowDeposit = _allowDeposit;
+    }
+
+    /**
+     * @notice Deposits a given amount of rHEGIC to the contract.
+     * @param amount Amount of rHEGIC to be deposited, i.e. amount of convHEGIC to
+     * be minted
+     */
+    function deposit(uint amount) external {
+        require(allowDeposit, "New deposits no longer accepted");
+        require(amount > 0, "Amount must be greater than zero");
+
+        if (userData[msg.sender].amountDeposited == 0) {
+            totalDepositors += 1;
+        }
+        totalDeposited += amount;
+
+        rHEGIC.safeTransferFrom(msg.sender, address(this), amount);
+        userData[msg.sender].amountDeposited += amount;
+
+        emit Deposited(msg.sender, amount);
+    }
+
+    /**
+     * @notice Claim a refund of rHEGIC before they are deposited to the redemption
+     * contract. The developer will notify users to do this if the project fails
+     * to attract enough deposit.
+     */
+    function claimRefund() external {
+        uint amount = userData[msg.sender].amountDeposited;
+
+        require(amount > 0, "User has not deposited any fund");
+        require(allowClaimRefund, "Funds already transferred to the redemption contract");
+
+        rHEGIC.safeTransfer(msg.sender, amount);
+
+        userData[msg.sender].amountDeposited = 0;
+        totalDeposited -= amount;
+        totalDepositors -= 1;
+
+        emit RefundClaimed(msg.sender, amount);
+    }
+
+    /**
+     * @notice Deposit all rHEGIC to the redemption contract. Once this is executed,
+     * no new deposit will be accepted, and users will not be able to claim rHEGIC refund.
+     */
+    function depositToRedemptionContract() external onlyOwner {
+        require(totalDeposited > 0, "No rHEGIC token to deposit");
+
+        rHEGIC.approve(address(redemption), totalDeposited);
+        redemption.deposit(totalDeposited);
+
+        allowDeposit = false;
+        allowClaimRefund = false;
+    }
+
+    // Functions to be overriden
+    function redeemAndStake() virtual external returns (uint) {}
+    function withdrawStakedHEGIC() virtual external {}
+    function getUserWithdrawableAmount(address account) virtual public view returns (uint) {}
+}
--- contracts/AutoStakeToSHegic.sol
+++ contracts/AutoStakeToSHegic.sol
@@ -1,55 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.5;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import "./interfaces/IHegicStakingPool.sol";
-import "./interfaces/IIOUTokenRedemption.sol";
-
+import "./AutoStake.sol";
 
 /**
  * @author Larrypc
- * @title Hegic AutoStake: to sHEGIC
+ * @title AutoStake
  * @notice Pools HegicIOUToken (rHEGIC) together and deposits to the rHEGIC --> HEGIC
- * redemption contract; withdraws HEGIC and deposits to jmonteer's HegicStakingPool at
- * regular intervals.
+ * redemption contract; withdraws HEGIC and deposits to jmonteer's hegicstakingpool.co
+ * at regular intervals.
  */
-contract AutoStakeToSHegic is Ownable {
+ contract AutoStakeToSHegic is AutoStake {
     using SafeMath for uint;
     using SafeERC20 for IERC20;
 
-    struct UserData {
-        uint amountDeposited;
-        uint amountWithdrawn;
-    }
-
-    IERC20 public immutable HEGIC;
-    IERC20 public immutable rHEGIC;
-    IHegicStakingPool public immutable sHEGIC;
-    IIOUTokenRedemption public immutable redemption;
-
-    uint public feeRate = 100;  // 1%
-    address public feeRecipient;
-
-    bool public allowDeposit = true;
-    bool public allowClaimRefund = true;
-
-    uint public totalDepositors = 0;
-    uint public totalDeposited = 0;
-    uint public totalRedeemed = 0;
-    uint public totalWithdrawable = 0;
-    uint public totalWithdrawn = 0;  // Not Including fees
-    uint public totalFeeCollected = 0;
-
-    uint public lastRedemptionTimestamp;
-
-    mapping(address => UserData) public userData;
-
-    event Deposited(address account, uint amount);
-    event RefundClaimed(address account, uint amount);
-    event Withdrawn(address account, uint amountAfterFee, uint fee);
+    IHegicStakingPool public sHEGIC;
 
     constructor(
         IERC20 _HEGIC,
@@ -67,96 +38,12 @@ contract AutoStakeToSHegic is Ownable {
     }
 
     /**
-     * @notice Set the fee rate users are charged upon withdrawal.
-     * @param _feeRate The new rate in basis points. E.g. 200 = 2%
-     */
-    function setFeeRate(uint _feeRate) external onlyOwner {
-        require(_feeRate >= 0, "Rate too low!");
-        require(_feeRate <= 500, "Rate too high!");
-        feeRate = _feeRate;
-    }
-
-    /**
-     * @notice Set the recipient address to fees generated.
-     * @param _feeRecipient The new recipient address
-     */
-    function setFeeRecipient(address _feeRecipient) external onlyOwner {
-        require(_feeRecipient != address(0), "Cannot set recipient to zero address");
-        feeRecipient = _feeRecipient;
-    }
-
-    /**
-     * @notice Set to accept or reject new deposits. May be called if the project
-     * fails to attract enough deposits that justify the work. In this case the
-     * developer will inform depositors to withdraw their rHEGIC by calling the
-     * `claimRefund` function.
-     * @param _allowDeposit Whether new deposits are accepted
-     */
-    function setAllowDeposit(bool _allowDeposit) external onlyOwner {
-        allowDeposit = _allowDeposit;
-    }
-
-    /**
-     * @notice Deposits a given amount of rHEGIC to the contract.
-     * @param amount Amount of rHEGIC to be deposited, i.e. amount of convHEGIC to
-     * be minted
-     */
-    function deposit(uint amount) external {
-        require(allowDeposit, "New deposits no longer accepted");
-        require(amount > 0, "Amount must be greater than zero");
-
-        if (userData[msg.sender].amountDeposited == 0) {
-            totalDepositors += 1;
-        }
-        totalDeposited += amount;
-
-        rHEGIC.safeTransferFrom(msg.sender, address(this), amount);
-        userData[msg.sender].amountDeposited += amount;
-
-        emit Deposited(msg.sender, amount);
-    }
-
-    /**
-     * @notice Claim a refund of rHEGIC before they are deposited to the redemption
-     * contract. The developer will notify users to do this if the project fails
-     * to attract enough deposit.
-     */
-    function claimRefund() external {
-        uint amount = userData[msg.sender].amountDeposited;
-
-        require(amount > 0, "User has not deposited any fund");
-        require(allowClaimRefund, "Funds already transferred to the redemption contract");
-
-        rHEGIC.safeTransfer(msg.sender, amount);
-
-        userData[msg.sender].amountDeposited = 0;
-        totalDeposited -= amount;
-        totalDepositors -= 1;
-
-        emit RefundClaimed(msg.sender, amount);
-    }
-
-    /**
-     * @notice Deposit all rHEGIC to the redemption contract. Once this is executed,
-     * no new deposit will be accepted, and users will not be able to claim rHEGIC refund.
-     */
-    function depositToRedemptionContract() external onlyOwner {
-        require(totalDeposited > 0, "No rHEGIC token to deposit");
-
-        rHEGIC.approve(address(redemption), totalDeposited);
-        redemption.deposit(totalDeposited);
-
-        allowDeposit = false;
-        allowClaimRefund = false;
-    }
-
-    /**
      * @notice Redeem the maximum possible amount of rHEGIC to HEGIC, then stake
      * in the sHEGIC contract. The developer will call this at regular intervals.
      * Anyone can call this as well, albeit no benefit.
      * @return amount Amount of HEGIC redeemed / sHEGIC staked in this transaction
      */
-    function redeemAndStake() external returns (uint amount) {
+    function redeemAndStake() override external returns (uint amount) {
         amount = redemption.redeem();
         HEGIC.approve(address(sHEGIC), amount);
         sHEGIC.deposit(amount);
@@ -168,22 +55,9 @@ contract AutoStakeToSHegic is Ownable {
     }
 
     /**
-     * @notice Calculate the maximum amount of sHEGIC token available for withdrawable
-     * by a user.
-     * @param account The user's account address
-     * @return amount The user's withdrawable amount
-     */
-    function getUserWithdrawableAmount(address account) public view returns (uint amount) {
-        amount = userData[account].amountDeposited.sub(userData[account].amountWithdrawn);
-        if (totalWithdrawable < amount) {
-            amount = totalWithdrawable;
-        }
-    }
-
-    /**
      * @notice Withdraw all available sHEGIC claimable by the user.
      */
-    function withdrawStakedHEGIC() public {
+    function withdrawStakedHEGIC() override external {
         uint amount = getUserWithdrawableAmount(msg.sender);
         require(amount > 0, "No sHEGIC token available for withdrawal");
 
@@ -202,5 +76,18 @@ contract AutoStakeToSHegic is Ownable {
         totalFeeCollected += fee;
 
         emit Withdrawn(msg.sender, amountAfterFee, fee);
+    }
+
+    /**
+     * @notice Calculate the maximum amount of sHEGIC token available for withdrawable
+     * by a user.
+     * @param account The user's account address
+     * @return amount The user's withdrawable amount
+     */
+    function getUserWithdrawableAmount(address account) override public view returns (uint amount) {
+        amount = userData[account].amountDeposited.sub(userData[account].amountWithdrawn);
+        if (totalWithdrawable < amount) {
+            amount = totalWithdrawable;
+        }
     }
 }
