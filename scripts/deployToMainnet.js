d4ba00d84515ee656821a168895b20d967e761c0
Larrypcdotcom <26318510+Larrypcdotcom@users.noreply.github.com>
2020-12-26 23:06:26 -0600

    Update mainnet deployment script

--- scripts/deployToMainnet.js
+++ scripts/deployToMainnet.js
@@ -1,36 +1,43 @@
 const { ethers } = require('hardhat');
 
 const addresses = {
-  HEGIC: '0x584bC13c7D411c00c01A62e8019472dE68768430',
-  rHEGIC: '0x47C0aD2aE6c0Ed4bcf7bc5b380D7205E89436e84',
-  sHEGIC: '0xf4128B00AFdA933428056d0F0D1d7652aF7e2B35',
-  zHEGIC: '0x837010619aeb2AE24141605aFC8f66577f6fb2e7',
+  WBTC:     '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+  HEGIC:    '0x584bC13c7D411c00c01A62e8019472dE68768430',
+  rHEGIC:   '0x47C0aD2aE6c0Ed4bcf7bc5b380D7205E89436e84',
+  sHEGIC:   '0xf4128B00AFdA933428056d0F0D1d7652aF7e2B35',
+  zHEGIC:   '0x837010619aeb2AE24141605aFC8f66577f6fb2e7',
   zLotPool: '0x9E4E091fC8921FE3575eab1c9a6446114f3b5Ef2',
-  redemption: ''  // TBD
+  GST:      ''  // TBD
 }
 
 const deploy = async () => {
-  const [ deployer ] = await ethers.getSigners();
-  console.log('Using account', deployer.address);
+  const [ deployer, recipient ] = await ethers.getSigners();
+  console.log(`Using account ${deployer.address} as deployer`);
+  console.log(`Using account ${recipient.address} as recipient`);
 
-  const AutoStakeToSHegic = await ethers.getContractFactory('AutoStakeToSHegic');
-  const AutoStakeToSHegicInstance = await AutoStakeToSHegic.deploy(
+  const AutoStakeForSHegic = await ethers.getContractFactory('AutoStakeForSHegic');
+  const AutoStakeForSHegicInstance = await AutoStakeForSHegic.deploy(
+    addresses.WBTC,
     addresses.HEGIC,
     addresses.rHEGIC,
     addresses.sHEGIC,
-    addresses.redemption,
+    addresses.GST,
+    200,
+    recipient.address
   );
-  console.log('AutoStakeToSHegic deployed to', AutoStakeToSHegicInstance.address);
+  console.log('AutoStakeForSHegic deployed to', AutoStakeForSHegicInstance.address);
 
-  const AutoStakeToZHegic = await ethers.getContractFactory('AutoStakeToZHegic');
-  const AutoStakeToZHegicInstance = await AutoStakeToZHegic.deploy(
+  const AutoStakeForZHegic = await ethers.getContractFactory('AutoStakeForZHegic');
+  const AutoStakeForZHegicInstance = await AutoStakeForZHegic.deploy(
     addresses.HEGIC,
     addresses.rHEGIC,
     addresses.zHEGIC,
     addresses.zLotPool,
-    addresses.redemption,
+    addresses.GST,
+    200,
+    recipient.address
   );
-  console.log('AutoStakeToZHegic deployed to', AutoStakeToZHegicInstance.address);
+  console.log('AutoStakeForZHegic deployed to', AutoStakeForZHegicInstance.address);
 };
 
 deploy()
