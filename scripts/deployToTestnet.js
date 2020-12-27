de82f0ad5c56fff5832a3d217d4cf1c78b7a913d
Larrypcdotcom <26318510+Larrypcdotcom@users.noreply.github.com>
2020-12-26 23:02:16 -0600

    Update testnet deployment script

--- scripts/deployToTestnet.js
+++ scripts/deployToTestnet.js
@@ -2,81 +2,87 @@ const { ethers } = require('hardhat');
 
 const deploy = async () => {
   const [ deployer, recipient ] = await ethers.getSigners();
-  console.log('Using account', deployer.address);
+  console.log(`Using account ${deployer.address} as deployer`);
+  console.log(`Using account ${recipient.address} as recipient`);
 
-  const FakeHegicToken = await ethers.getContractFactory('FakeHegicToken');
-  const FakeHegicTokenInstance = await FakeHegicToken.deploy();
-  console.log('FakeHegicToken deployed to', FakeHegicTokenInstance.address);
+  //--------------------
+  // Fake tokens
+  //--------------------
 
-  const FakeRHegicToken = await ethers.getContractFactory('FakeRHegicToken');
-  const FakeRHegicTokenInstance = await FakeRHegicToken.deploy();
-  console.log('FakeRHegicToken deployed to', FakeRHegicTokenInstance.address);
+  const FakeWBTC = await ethers.getContractFactory('FakeWBTC');
+  const FakeWBTCInstance = await FakeWBTC.deploy();
+  console.log('FakeWBTC deployed to', FakeWBTCInstance.address);
 
-  const FakeHegicStakingPool = await ethers.getContractFactory('FakeHegicStakingPool');
-  const FakeHegicStakingPoolInstance = await FakeHegicStakingPool.deploy(
-    FakeHegicTokenInstance.address
-  );
-  console.log('FakeHegicStakingPool deployed to', FakeHegicStakingPoolInstance.address);
+  const FakeHegic = await ethers.getContractFactory('FakeHegic');
+  const FakeHegicInstance = await FakeHegic.deploy();
+  console.log('FakeHegic deployed to', FakeHegicInstance.address);
 
-  const FakeZHegicToken = await ethers.getContractFactory('FakeZHegicToken');
-  const FakeZHegicTokenInstance = await FakeZHegicToken.deploy();
-  console.log('FakeZHegicToken deployed to', FakeZHegicTokenInstance.address);
+  const FakeRHegic = await ethers.getContractFactory('FakeRHegic');
+  const FakeRHegicInstance = await FakeRHegic.deploy();
+  console.log('FakeRHegic deployed to', FakeRHegicInstance.address);
 
-  const FakeHegicPoolV2 = await ethers.getContractFactory('FakeHegicPoolV2');
-  const FakeHegicPoolV2Instance = await FakeHegicPoolV2.deploy(
-    FakeHegicTokenInstance.address,
-    FakeZHegicTokenInstance.address,
+  //--------------------
+  // Fake staking pools
+  //--------------------
+
+  const FakeSHegic = await ethers.getContractFactory('FakeSHegic');
+  const FakeSHegicInstance = await FakeSHegic
+    .deploy(FakeHegicInstance.address, FakeWBTCInstance.address);
+  console.log('FakeSHegic deployed to', FakeSHegicInstance.address);
+
+  const FakeZHegic = await ethers.getContractFactory('FakeZHegic');
+  const FakeZHegicInstance = await FakeZHegic.deploy();
+  console.log('FakeZHegic deployed to', FakeZHegicInstance.address);
+
+  const FakeZLotPool = await ethers.getContractFactory('FakeZLotPool');
+  const FakeZLotPoolInstance = await FakeZLotPool.deploy(
+    FakeHegicInstance.address,
+    FakeZHegicInstance.address,
     1
   );
-  console.log('FakeHegicPoolV2 deployed to', FakeHegicPoolV2Instance.address);
+  console.log('FakeZLotPool deployed to', FakeZLotPoolInstance.address);
 
-  const IOUTokenRedemption = await ethers.getContractFactory('IOUTokenRedemption');
-  const IOUTokenRedemptionInstance = await IOUTokenRedemption.deploy(
-    FakeRHegicTokenInstance.address,
-    FakeHegicTokenInstance.address,
-    200  // ~50 min for 15 sec block time
+  //--------------------
+  // GST
+  //--------------------
+
+  const GradualTokenSwap = await ethers.getContractFactory('GradualTokenSwapUpgradable');
+  const GradualTokenSwapInstance = await GradualTokenSwap
+    .deploy(
+      await ethers.provider.getBlockNumber() + 14,
+      1000,
+      FakeRHegicInstance.address,
+      FakeHegicInstance.address
+    );
+  console.log('GradualTokenSwap deployed to', GradualTokenSwapInstance.address);
+
+  //--------------------
+  // AutoStake contract
+  //--------------------
+
+  const AutoStakeForSHegic = await ethers.getContractFactory('AutoStakeForSHegic');
+  const AutoStakeForSHegicInstance = await AutoStakeForSHegic.deploy(
+    FakeWBTCInstance.address,
+    FakeHegicInstance.address,
+    FakeRHegicInstance.address,
+    FakeSHegicInstance.address,
+    GradualTokenSwapInstance.address,
+    200,
+    recipient.address
   );
-  console.log('IOUTokenRedemption deployed to', IOUTokenRedemptionInstance.address);
+  console.log('AutoStakeForSHegic deployed to', AutoStakeForSHegicInstance.address);
 
-  const AutoStakeToSHegic = await ethers.getContractFactory('AutoStakeToSHegic');
-  const AutoStakeToSHegicInstance = await AutoStakeToSHegic.deploy(
-    FakeHegicTokenInstance.address,
-    FakeRHegicTokenInstance.address,
-    FakeHegicStakingPoolInstance.address,
-    IOUTokenRedemptionInstance.address,
+  const AutoStakeForZHegic = await ethers.getContractFactory('AutoStakeForZHegic');
+  const AutoStakeForZHegicInstance = await AutoStakeForZHegic.deploy(
+    FakeHegicInstance.address,
+    FakeRHegicInstance.address,
+    FakeZHegicInstance.address,
+    FakeZLotPoolInstance.address,
+    GradualTokenSwapInstance.address,
+    200,
+    recipient.address
   );
-  console.log('AutoStakeToSHegic deployed to', AutoStakeToSHegicInstance.address);
-
-  const AutoStakeToZHegic = await ethers.getContractFactory('AutoStakeToZHegic');
-  const AutoStakeToZHegicInstance = await AutoStakeToZHegic.deploy(
-    FakeHegicTokenInstance.address,
-    FakeRHegicTokenInstance.address,
-    FakeZHegicTokenInstance.address,
-    FakeHegicPoolV2Instance.address,
-    IOUTokenRedemptionInstance.address,
-  );
-  console.log('AutoStakeToZHegic deployed to', AutoStakeToZHegicInstance.address);
-
-  // Set fee rate and recipient
-  await AutoStakeToSHegicInstance.connect(deployer)
-    .setFeeRate(200);  // 2%
-
-  await AutoStakeToSHegicInstance.connect(deployer)
-    .setFeeRecipient(recipient.address);
-
-  await AutoStakeToZHegicInstance.connect(deployer)
-    .setFeeRate(200);
-
-  await AutoStakeToZHegicInstance.connect(deployer)
-    .setFeeRecipient(recipient.address);
-
-  // Set pool address for Fake zHEGIC token
-  await FakeZHegicTokenInstance.connect(deployer)
-    .setPool(FakeHegicPoolV2Instance.address);
-
-  // Fund redemption contract with Fake HEGIC token
-  await FakeHegicTokenInstance.connect(deployer)
-    .transfer(IOUTokenRedemptionInstance.address, '100000000000000000000');
+  console.log('AutoStakeForZHegic deployed to', AutoStakeForZHegicInstance.address);
 };
 
 deploy()
